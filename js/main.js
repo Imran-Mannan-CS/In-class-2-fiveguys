ed1594f4e3d2225677aaa8a37c41b1a304f8e702
Imran-Mannan-CS <imranmannan1278@gmail.com>
2020-10-30 18:49:09 -0600

    Completed assesment 2

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(".overlay").hide();
+
+$(".menu-btn").click(function(e){
+  $(".menu-btn i").toggleClass("fa-bars fa-times-circle");
+  $("nav").toggle();
+  e.preventDefault();
+})
+
+$(".menu-locations").click(function(e){
+  $(".overlay").toggle();
+  e.preventDefault();
+})
+
+$(".locations a").click(function(e){
+  $(".overlay").toggle();
+  e.preventDefault();
+})
